798830711a1914c9143ab9589e9d4ef34112f733
DelacourMelchior <delacour.melchior@gmail.com>
2026-01-08 22:25:05 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,21 +5,13 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      // AJOUTER CETTE LIGNE : Indique à GitHub que le site est à la racine du domaine
       base: '/', 
-      
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
       plugins: [react()],
       define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
       },
       resolve: {
         alias: {
-          // Permet d'utiliser @ dans votre code pour faire référence au dossier racine
           '@': path.resolve(__dirname, '.'),
         }
       }
